9f3a93b13be162d393b7efb8d1c6884a84acd999
Raharjo Motor Tuban <raharjomotorbinangun@gmail.com>
2025-12-01 01:27:37 +0700

    Deploy final project RAHARJO MOTOR TUBAN

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// Konfigurasi Firebase kamu
+const firebaseConfig = {
+    apiKey: "AIzaSyASx1L6_KVL_S0XJfb5Oh3FwM0cOYTronQ",
+    authDomain: "raharjo-motor-tuban.firebaseapp.com",
+    projectId: "raharjo-motor-tuban",
+    storageBucket: "raharjo-motor-tuban.firebasestorage.app",
+    messagingSenderId: "392547240984",
+    appId: "1:392547240984:web:8c7ec5f0b25225a066c76d",
+    measurementId: "G-WBPLNJ4ZGT"
+};
+
+// Inisialisasi Firebase
+firebase.initializeApp(firebaseConfig);
